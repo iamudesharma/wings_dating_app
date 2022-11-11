d3a18a5d911cf7eeb1f97912dd8e474737fe9e28
iamudesharma <udesh2568@gmail.com>
2022-11-11 22:16:52 +0530

    repo exception added

---
+++ lib/repo/repo_exception.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:io';
+
+// import 'package:appwrite/appwrite.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../helpers/logger.dart';
+// import 'package:shopping_admin/helpers/logger.dart';
+
+class RepositoryException implements Exception {
+  final String message;
+  final Exception? exception;
+  final StackTrace? stackTrace;
+
+  RepositoryException({required this.message, this.exception, this.stackTrace});
+
+  @override
+  String toString() {
+    return "RepositoryExcept :$message";
+  }
+}
+
+mixin RepositoryExceptionMixin {
+  Future<T> exceptionHandler<T>(Future computation,
+      {String unKnownMessage = "Exception"}) async {
+    try {
+      return await computation;
+    } on FirebaseAuthException catch (e) {
+      logger.e(e);
+
+      Fluttertoast.showToast(msg: e.message ?? "An Firebase Auth error");
+      throw RepositoryException(
+        message: e.message ?? "An Firebase Auth error",
+      );
+    } on FirebaseException catch (e) {
+      logger.e(e);
+
+      Fluttertoast.showToast(msg: e.message ?? "An Firebase Auth error");
+
+      throw RepositoryException(
+        message: e.message ?? "An Firebase  error",
+      );
+    } on IOException catch (e, st) {
+      logger.e(e);
+      Fluttertoast.showToast(msg: e.toString());
+
+      throw RepositoryException(
+          message: unKnownMessage, exception: e, stackTrace: st);
+    } on Exception catch (e, st) {
+      logger.e(e, [st]);
+      Fluttertoast.showToast(msg: e.toString());
+
+      throw RepositoryException(
+          message: unKnownMessage, exception: e, stackTrace: st);
+    }
+  }
+}
