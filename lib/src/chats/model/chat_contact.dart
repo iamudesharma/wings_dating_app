45cc7c7f8a18c61614790e912b01c3c8bec83f79
iamudesharma <udesh2568@gmail.com>
2023-01-06 11:38:00 +0530

    chat contact model added

---
+++ lib/src/chats/model/chat_contact.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'chat_contact.g.dart';
+part 'chat_contact.freezed.dart';
+
+@freezed
+class ChatContact with _$ChatContact {
+  const factory ChatContact({
+    required String name,
+    required String profilePic,
+    required String contactId,
+    required DateTime timeSent,
+    required String lastMessage,
+    required String fcmToken,
+  }) = _ChatContact;
+
+  factory ChatContact.fromJson(Map<String, dynamic> json) =>
+      _$ChatContactFromJson(json);
+}
