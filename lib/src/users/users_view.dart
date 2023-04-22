b9ae84fb4a94f21c80cc29a7323d3f88e7faabd7
iamudesharma <udesh2568@gmail.com>
2023-04-22 22:19:30 +0530

    PlatformScaffold added

--- lib/src/users/users_view.dart
+++ lib/src/users/users_view.dart
@@ -9,6 +9,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geoflutterfire2/geoflutterfire2.dart';
 import 'package:intl/intl.dart';
@@ -109,6 +110,20 @@ class _UsersViewState extends ConsumerState<UsersView>
 
     appState = WidgetsBinding.instance.lifecycleState;
     WidgetsBinding.instance.addObserver(this);
+
+    saveCubeUserInFirebase();
+  }
+
+  saveCubeUserInFirebase() async {
+    final userModel =
+        ref.read(ProfileController.userControllerProvider).userModel?.cubeUser;
+
+    SharedPrefs sharedPrefs = await SharedPrefs.instance.init();
+    CubeUser? user = sharedPrefs.getUser();
+
+    if (userModel!.id == null) {
+      await ref.read(profileRepoProvider).updateCubeUserDoc(user!);
+    }
   }
 
   @override
@@ -198,7 +213,7 @@ class _UsersViewState extends ConsumerState<UsersView>
         ref.watch(ProfileController.userControllerProvider).userModel;
     final userList = ref.watch(userListProvider);
 
-    return Scaffold(
+    return PlatformScaffold(
       body: LayoutBuilder(builder: (context, constraints) {
         return RefreshIndicator(
           onRefresh: () async => ref.refresh(userListProvider),
