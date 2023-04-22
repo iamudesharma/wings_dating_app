0215654d622d0c60188c17b71402efab26e45d47
iamudesharma <udesh2568@gmail.com>
2023-04-22 22:18:55 +0530

    flutter_platform_widgets

--- lib/src/auth/sign_options.dart
+++ lib/src/auth/sign_options.dart
@@ -1,17 +1,13 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:connectycube_sdk/connectycube_sdk.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 // import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wings_dating_app/const/pref_util.dart';
-import 'package:wings_dating_app/helpers/helpers.dart';
-import 'package:wings_dating_app/repo/profile_repo.dart';
 // import 'package:flutterfire_ui/auth.dart';
 import 'package:wings_dating_app/routes/app_router.dart';
 
-import '../../firebase_options.dart';
 // import 'package:firebase_auth/firebase_auth.dart' show EmailAuthProvider;
 
 @RoutePage()
@@ -20,55 +16,57 @@ class SignOptionsView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    return SignInScreen(
-      oauthButtonVariant: OAuthButtonVariant.icon_and_text,
-      headerBuilder: (context, constraints, shrinkOffset) {
-        return SizedBox(
-          height: constraints.maxHeight,
-          width: constraints.maxWidth,
-          child: const Center(
-            child: Text(
-              'Wings Dating',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+    return PlatformScaffold(
+      body: SignInScreen(
+        oauthButtonVariant: OAuthButtonVariant.icon_and_text,
+        headerBuilder: (context, constraints, shrinkOffset) {
+          return SizedBox(
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
+            child: const Center(
+              child: Text(
+                'Wings Dating',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        );
-      },
-      auth: FirebaseAuth.instance,
-      actions: [
-        AuthStateChangeAction<SignedIn>((context, state) async {
-          // state.user?.getIdToken().then((idToken) {
-          //   signInUsingFirebase(
-          //           DefaultFirebaseOptions.currentPlatform.projectId, idToken)
-          //       .then((cubeUser) async {
-          //     await SharedPrefs.instance.saveNewUser(cubeUser);
-
-          //     logger.i('Saved  cubeUser: is true ');
-          AutoRouter.of(context).replace(
-            const DashboardRoute(),
           );
-          // });
-          // });
-        }),
-        AuthStateChangeAction<SigningUp>((context, state) async {
-          // await    state!.user?.getIdToken().then((idToken) {
-          //       signInUsingFirebase(
-          //               DefaultFirebaseOptions.currentPlatform.projectId, idToken)
-          //           .then((cubeUser) async {
-          //         await SharedPrefs.instance.saveNewUser(cubeUser);
+        },
+        auth: FirebaseAuth.instance,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) async {
+            // state.user?.getIdToken().then((idToken) {
+            //   signInUsingFirebase(
+            //           DefaultFirebaseOptions.currentPlatform.projectId, idToken)
+            //       .then((cubeUser) async {
+            //     await SharedPrefs.instance.saveNewUser(cubeUser);
 
-          //         logger.i('Saved  cubeUser: is true ');
-          //         AutoRouter.of(context).replace(
-          //           const DashboardRoute(),
-          //         );
-          //       });
-          // });
-        }),
-      ],
-      showAuthActionSwitch: true,
+            //     logger.i('Saved  cubeUser: is true ');
+            AutoRouter.of(context).replace(
+              const DashboardRoute(),
+            );
+            // });
+            // });
+          }),
+          AuthStateChangeAction<SigningUp>((context, state) async {
+            // await    state!.user?.getIdToken().then((idToken) {
+            //       signInUsingFirebase(
+            //               DefaultFirebaseOptions.currentPlatform.projectId, idToken)
+            //           .then((cubeUser) async {
+            //         await SharedPrefs.instance.saveNewUser(cubeUser);
+
+            //         logger.i('Saved  cubeUser: is true ');
+            //         AutoRouter.of(context).replace(
+            //           const DashboardRoute(),
+            //         );
+            //       });
+            // });
+          }),
+        ],
+        showAuthActionSwitch: true,
+      ),
     );
   }
 }
