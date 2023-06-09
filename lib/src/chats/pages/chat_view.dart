ca7e0717d7612d2ad5835973490fd9ed22bcee8e
iamudesharma <udesh2568@gmail.com>
2023-06-09 17:23:08 +0530

    message bar fixed

--- lib/src/chats/pages/chat_view.dart
+++ lib/src/chats/pages/chat_view.dart
@@ -1140,7 +1140,7 @@ class ChatScreenState extends ConsumerState<ChatScreen> {
           onPressed: () {},
         ),
       ],
-      messageBarColor: Colors.black,
+      messageBarColor: Colors.white12,
       onSend: (p0) async {
         if (p0.isNotEmpty) {
           onSendChatMessage(p0);
@@ -1406,3 +1406,172 @@ String addEmotionsCase(Emotions emotions) => switch (emotions) {
       Emotions.love => "love",
       Emotions.wow => "wow",
     };
+
+class MessageBar extends StatelessWidget {
+  final bool replying;
+  final String replyingTo;
+  final List<Widget> actions;
+  final TextEditingController _textController = TextEditingController();
+  final Color replyWidgetColor;
+  final Color replyIconColor;
+  final Color replyCloseColor;
+  final Color messageBarColor;
+  final Color sendButtonColor;
+  final void Function(String)? onTextChanged;
+  final void Function(String)? onSend;
+  final void Function()? onTapCloseReply;
+
+  /// [MessageBar] constructor
+  ///
+  ///
+  MessageBar({
+    this.replying = false,
+    this.replyingTo = "",
+    this.actions = const [],
+    this.replyWidgetColor = const Color(0xffF4F4F5),
+    this.replyIconColor = Colors.blue,
+    this.replyCloseColor = Colors.black12,
+    this.messageBarColor = const Color(0xffF4F4F5),
+    this.sendButtonColor = Colors.blue,
+    this.onTextChanged,
+    this.onSend,
+    this.onTapCloseReply,
+  });
+
+  /// [MessageBar] builder method
+  ///
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            replying
+                ? Container(
+                    color: replyWidgetColor,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.reply,
+                          color: replyIconColor,
+                          size: 24,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              'Re : ' + replyingTo,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: onTapCloseReply,
+                          child: Icon(
+                            Icons.close,
+                            color: replyCloseColor,
+                            size: 24,
+                          ),
+                        ),
+                      ],
+                    ))
+                : Container(),
+            replying
+                ? Container(
+                    height: 1,
+                    color: Colors.grey.shade300,
+                  )
+                : Container(),
+            Container(
+              color: messageBarColor,
+              padding: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
+              ),
+              child: Row(
+                children: <Widget>[
+                  ...actions,
+                  Expanded(
+                    child: Container(
+                      child: PlatformTextField(
+                        controller: _textController,
+                        keyboardType: TextInputType.multiline,
+                        textCapitalization: TextCapitalization.sentences,
+                        minLines: 1,
+                        maxLines: 3,
+                        onChanged: onTextChanged,
+                        cupertino: (context, platform) =>
+                            CupertinoTextFieldData(
+                                placeholder: "Type your message here",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.white,
+                                  ),
+                                  borderRadius: BorderRadius.circular(30.0),
+                                )),
+                        material: (context, platform) => MaterialTextFieldData(
+                          decoration: InputDecoration(
+                            hintText: "Type your message here",
+                            hintMaxLines: 1,
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 10),
+                            hintStyle: TextStyle(
+                              fontSize: 16,
+                            ),
+                            // fillColor: Colors.white,
+                            // filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                                width: 0.2,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(
+                                color: Colors.black26,
+                                width: 0.2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: InkWell(
+                      child: Icon(
+                        Icons.send,
+                        color: sendButtonColor,
+                        size: 24,
+                      ),
+                      onTap: () {
+                        if (_textController.text.trim() != '') {
+                          if (onSend != null) {
+                            onSend!(_textController.text.trim());
+                          }
+                          _textController.text = '';
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
