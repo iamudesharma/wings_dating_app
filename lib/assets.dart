d683b41aca735f999e8066973188b8e7761aa591
iamudesharma <udesh2568@gmail.com>
2023-06-21 13:39:19 +0530

    assets generate added

---
+++ lib/assets.dart
@@ -0,0 +1,10 @@
+
+class Assets {
+  Assets._();
+
+
+  final logoPNG = 'assets/logo.png';
+  final rainbow2SVG = 'assets/rainbow-2.svg';
+  final rainbow3SVG = 'assets/rainbow-3.svg';
+  final rainbowJSON = 'assets/rainbow.json';
+}
